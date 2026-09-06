4c30db720cc9edc5c7b59bad5ea740b1f4cf48aa
Sebastian Freundt <freundt@ga-group.nl>
2026-09-06 06:29:32 +0000

    fix, speak turtle when emitting @base decls

--- src/ttl2ttl.c
+++ src/ttl2ttl.c
@@ -553,12 +553,12 @@ decl(void *usr, ttl_iri_t decl)
 		}
 		if (decl.pre.str) {
 			swrit("@prefix ", 8U, stdi);
+			swrit(decl.pre.str, decl.pre.len, stdi);
+			sputc(':', stdi);
+			sputc(' ', stdi);
 		} else {
 			swrit("@base ", 6U, stdi);
 		}
-		swrit(decl.pre.str, decl.pre.len, stdi);
-		sputc(':', stdi);
-		sputc(' ', stdi);
 		sputc('<', stdi);
 		swrit(decl.val.str, decl.val.len, stdi);
 		sputc('>', stdi);
